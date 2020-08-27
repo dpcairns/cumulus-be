73497eee224ef2ca17f02256608c00e39fc0502d
Heather Peterson <hpeterson462@gmail.com>
2020-08-27 12:57:59 -0700

    added search test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -111,6 +111,30 @@ describe('routes', () => {
     done();
   });
 
+  test('returns weather for the user when hitting GET /api/search/', async (done) => {
+
+    const expectWeather = {
+      'location': 'Ashland',
+      'country_code': 'US',
+      'state_code': 'OR',
+      'uv': 2.68429,
+      'weather_description': 'Clear sky',
+      'timezone': 'America/Los_Angeles',
+      'temp': 24,
+      'sunrise': '13:32',
+      'sunset': '02:52',
+      'lat': 42.19458,
+      'lon': -122.70948
+    };
+    const data = await fakeRequest(app)
+      .get('/api/search?key=b0dbcac67e29478a8215f76b13d1f54e&city=ashland&state=oregon&country=us')
+      .set('Authorization', token)
+      .expect('Content-Type', /json/)
+      .expect(200);
+    expect(data.body).toEqual(expectWeather);
+    done();
+  });
+
 
 
 });
