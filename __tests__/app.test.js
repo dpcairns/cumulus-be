c22cefcbc03bb8cafc92877e47eeb8f61b73963a
Heather Peterson <hpeterson462@gmail.com>
2020-08-27 12:01:57 -0700

    tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,64 +1,66 @@
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+
 require('dotenv').config();
-
 const { execSync } = require('child_process');
-
 const fakeRequest = require('supertest');
 const app = require('../lib/app');
 const client = require('../lib/client');
-
-describe('app routes', () => {
-  describe('routes', () => {
-    let token;
-  
-    beforeAll(async done => {
-      execSync('npm run setup-db');
-  
-      client.connect();
-  
-      const signInData = await fakeRequest(app)
-        .post('/auth/signup')
-        .send({
-          email: 'jon@user.com',
-          password: '1234'
-        });
-      
-      token = signInData.body.token;
-  
-      return done();
-    });
-  
-    afterAll(done => {
-      return client.end(done);
-    });
-
-  test('returns animals', async() => {
-
-    const expectation = [
-      {
-        'id': 1,
-        'name': 'bessie',
-        'coolfactor': 3,
-        'owner_id': 1
-      },
-      {
-        'id': 2,
-        'name': 'jumpy',
-        'coolfactor': 4,
-        'owner_id': 1
-      },
-      {
-        'id': 3,
-        'name': 'spot',
-        'coolfactor': 10,
-        'owner_id': 1
-      }
-    ];
-
+describe('routes', () => {
+  let token;
+  const newWeather = {
+    location: '',
+    state_code: '',
+    country_code: '',
+    lat: '',
+    lon: ''
+  };
+  beforeAll(async done => {
+    execSync('npm run setup-db');
+    client.connect();
+    const signInData = await fakeRequest(app)
+      .post('/auth/signup')
+      .send({
+        email: 'jon@user.com',
+        password: '1234'
+      });
+    token = signInData.body.token;
+    return done();
+  });
+  afterAll(done => {
+    return client.end(done);
+  });
+  test('returns a new weather when creating new request', async (done) => {
     const data = await fakeRequest(app)
-      .get('/animals')
+      .post('/api/weather')
+      .send(newWeather)
+      .set('Authorization', token)
       .expect('Content-Type', /json/)
       .expect(200);
-
-    expect(data.body).toEqual(expectation);
+    expect(data.body).toEqual(newWeather);
+    done();
   });
-})
+  test('returns all detail of favorite weather for the user when hitting GET /api/weather/:id', async (done) => {
+    const expected = [
+      {
+        country_code: '',
+        lat: '',
+        location: '',
+        lon: '',
+        state_code: '',
+        id: 2,
+        user_id: 2
+      },
+    ];
+    const data = await fakeRequest(app)
+      .get('/api/guitars')
+      .set('Authorization', token)
+      .expect('Content-Type', /json/)
+      .expect(200);
+    expect(data.body).toEqual(expected);
+    done();
+  });
+});
+
